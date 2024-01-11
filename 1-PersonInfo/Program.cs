0984188533260fb8db9c7b52fd379653aee17b6d
Reza Dehghani <rdehghani.akorn@gmail.com>
2024-01-11 20:06:18 +0330

    Added TransfareInformation and chenged programclass

--- 1-PersonInfo/Program.cs
+++ 1-PersonInfo/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+
+
+using _1_PersonInfo;
+
+while (true)
+{
+    try
+    {
+        int execute = TransfareInformation.Run();
+        switch (execute)
+        {
+            case 1:
+                {
+                    var name = TransfareInformation.GetString("enter name");
+                    var LastName = TransfareInformation.GetString("ente last name");
+                    TransfareInformation.SavePersonInfo(name, LastName);
+                    var fullName = TransfareInformation.GetFullName(name, LastName);
+                    Console.WriteLine($"------------\n{fullName}\n------------");
+                    break;
+                }
+            case 0:
+                {
+                    Environment.Exit(0);
+                    break;
+                }
+            default:
+                {
+                    TransfareInformation.Run();
+                    break;
+                }
+        }
+    }
+    catch (Exception ex)
+    {
+        TransfareInformation.Error(ex.Message);
+    }
+}
+
---
+++ 1-PersonInfo/TransfareInformation.cs
@@ -0,0 +1,47 @@
+﻿namespace _1_PersonInfo;
+
+public abstract class TransfareInformation
+{
+    static List<Person> persons = new();
+
+    public static void SavePersonInfo(string name, string lastName)
+    {
+        Person personInfo = new Person(name, lastName);
+        personInfo.GenerateFullName();
+        persons.Add(personInfo);
+    }
+
+    public static string GetFullName(string name, string lastName)
+    {
+        var personn = persons.Find(x => x.Name == name && x.LastName == lastName);
+        if (personn == null)
+        {
+            throw new Exception();
+        }
+        return personn.FullName;
+    }
+   public static string GetString(string message)
+    {
+        Console.WriteLine(message);
+        string value = Console.ReadLine()!;
+        return value;
+    }
+
+
+    public static int Run()
+    {
+        int run;
+        Console.WriteLine("\n 1: start\n 0: End");
+        run = int.Parse(Console.ReadLine()!);
+        return run;
+    }
+
+    public static void Error(string message)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine($"------------\n{message}\n-----------");
+        Console.ResetColor();
+    }
+
+}
+
