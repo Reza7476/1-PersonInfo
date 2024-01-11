200db1c00dba8310bf0ec3f73e2a5a6c71023a1a
Reza Dehghani <rdehghani.akorn@gmail.com>
2024-01-11 19:59:29 +0330

    Added Person Class

---
+++ 1-PersonInfo/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace _1_PersonInfo
+{
+    public class Person
+    {
+        public Person(string name, string lastName)
+        {
+            Name = name;
+            LastName = lastName;
+        }
+        public string  Name { get; set; }
+        public string  LastName { get; set; }
+        public string?  FullName { get; private set; }
+       
+        
+        public void GenerateFullName()
+        {
+            FullName = Name + "  " + LastName;
+        }
+    }
+}
